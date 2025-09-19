86359eed0dd287239e5db8cb6563fc611f28d24b
GUSTAVO PEREIRA GARBIM <gustavo.garbim@websupply.com.br>
2025-09-19 13:30:25 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from "./components/Header.vue"
+</script>
+
+<template>
+  <main>
+  <header class="border-1 border-indigo-600 h-20 bg-white "> <Header /> </header>
+
+  <section class="border-1 border-indigo-600 h-187"> SECTION </section>
+
+  <footer class="border-1 border-indigo-600 h-20"> FOOTER </footer>
+  </main>
+</template>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <main class="flex justify-start">
+        <button class="p-5"> Add Product </button>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
