90da1b4de185f8358fa933dc926cfa998f0d93a6
GUSTAVO PEREIRA GARBIM <gustavogarbim850@gmail.com>
2025-09-30 11:44:17 -0300

    feat: SearchBar added

--- src/view/Section.vue
+++ src/view/Section.vue
@@ -1,29 +1,53 @@
 <template>
   <main class="h-screen w-auto bg-gradient-to-b from-sky-100 to-sky-300">
     <Header />
+    <SearchBar />
     <div class="ml-10 mt-10">
       <h1 class="text-3xl font-bold text-gray-800 mb-2">Featured Products</h1>
       <h2 class="font-medium text-lg text-gray-600">
         Discover our premium collection of products
       </h2>
     </div>
-    <div class="grid grid-cols-4 gap-8 mt-10">
+    <div class="px-10 mt-6">
+      <input
+      type="text"
+      v-model="searchTerm"
+      placeholder="Search products..."
+      class="ml-140 w-180 p-3 border border-gray-300 rounded-lg"
+    />
+    </div>
+    <div class="grid grid-cols-4 gap-8  p-10">
       <CardProduct
-        v-for="product in products"
+        v-for="product in filteredProducts"
         :key="product.id"
         :product="product"
+        @produtoAtualizado="pullDataProduct"
       />
     </div>
   </main>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import api from "../services/api";
 import Header from "../components/Header.vue";
 import CardProduct from "../components/CardProduct.vue";
+import SearchBar from "../components/SearchBar.vue";
 
 const products = ref([]);
+const searchTerm = ref("");
+
+const filteredProducts = computed(() => {
+  if (!searchTerm.value) {
+    return products.value;
+  }
+
+  return products.value.filter(
+    (
+      product // Converte o nome do produto e o termo de busca para minúsculas para uma busca case-insensitive
+    ) => product.name.toLowerCase().includes(searchTerm.value.toLowerCase())
+  );
+});
 
 const pullDataProduct = async () => {
   try {
