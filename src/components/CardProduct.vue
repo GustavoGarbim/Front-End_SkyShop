4d9bc26a99507e5c750e56d94f3573ddcd24a65b
GUSTAVO PEREIRA GARBIM <gustavogarbim850@gmail.com>
2025-09-30 11:16:47 -0300

    feat: page for details of product

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -5,8 +5,13 @@
       :productId="selectedProductId"
       @fechar="fecharEditProductModal"
     />
+    <DetailsProductModal
+      :visivel="isModalVisibleDetails"
+      :productId="selectedProductIdDetails"
+      @fechar="fecharDetailsProductModal"
+    />
     <div
-      className="w-90 h-87 ml-10 mt-10 bg-white rounded-xl shadow-md hover:shadow-xl transition-all duration-300 overflow-hidden group hover:-translate-y-1 p-1"
+      className="w-90 h-90 ml-10 mt-10 bg-white rounded-xl shadow-md hover:shadow-xl transition-all duration-300 overflow-hidden group hover:-translate-y-1 p-1"
     >
       <div className="relative overflow-hidden flex justify-center">
         <img
@@ -17,6 +22,12 @@
         <div
           className="absolute top-2 right-2 flex gap-1 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
         >
+          <button
+            className="hover:cursor-pointer bg-blue-500 hover:bg-blue-600 text-white p-2 rounded-full transition-colors duration-200"
+            @click="abrirDetailsProductModal(product)"
+          >
+            👁️
+          </button>
           <button
             className="hover:cursor-pointer bg-blue-500 hover:bg-blue-600 text-white p-2 rounded-full transition-colors duration-200"
             @click="abrirEditProductModal(product)"
@@ -36,7 +47,7 @@
         <h3 class="text-xl font-semibold text-gray-800 mb-2 truncate">
           {{ product.name }}
         </h3>
-        <p className="text-gray-600 text-sm mb-4 line-clamp-2">
+        <p className="text-gray-600 text-sm mb-4 line-clamp-2 h-10">
           {{ product.description }}
         </p>
 
@@ -45,10 +56,17 @@
             >R$ {{ (product.price || 0).toFixed(2) }}</span
           >
           <button
+            v-if="product.stock"
             @click="cartStore.addToCart(product)"
             className="hover:cursor-pointer bg-sky-500 hover:bg-sky-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 transition-all duration-200 transform hover:scale-105"
           >
             Add to Cart
+          </button>
+          <button
+            v-else
+            className="hover:cursor-pointer bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg flex items-center gap-2 transition-all duration-200 transform hover:scale-105"
+          >
+            Out of Stock
           </button>
         </div>
       </div>
@@ -61,6 +79,7 @@ import { ref } from "vue";
 import { useCartStore } from "../stores/cartStore";
 import api from "../services/api";
 import EditProductModal from "./EditProductModal.vue";
+import DetailsProductModal from "./DetailsProductModal.vue";
 
 const cartStore = useCartStore();
 
@@ -71,9 +90,22 @@ const props = defineProps({
   },
 });
 
-
 const isModalVisible = ref(false);
 const selectedProductId = ref(null);
+
+const isModalVisibleDetails = ref(false);
+const selectedProductIdDetails = ref(null);
+
+const abrirDetailsProductModal = (product) => {
+  console.log(`[CardProduct] Abrindo modal para o produto ID: ${product.id}`);
+  selectedProductIdDetails.value = product.id;
+  isModalVisibleDetails.value = true;
+};
+
+const fecharDetailsProductModal = () => {
+  isModalVisibleDetails.value = false;
+  selectedProductIdDetails.value = null;
+};
 
 const abrirEditProductModal = (product) => {
   console.log(`[CardProduct] Abrindo modal para o produto ID: ${product.id}`);
---
+++ src/components/DetailsProductModal.vue
@@ -0,0 +1,147 @@
+<template>
+  <main>
+    <div
+      v-if="visivel"
+      class="fixed top-0 left-0 w-screen h-screen bg-black/50 flex justify-center items-center z-50"
+    >
+      <div
+        class="bg-white p-5 rounded-2xl shadow-2xl w-230 h-180 flex flex-col"
+      >
+        <div
+          class="flex justify-between items-center p-6 border-b border-gray-200"
+        >
+          <h1 class="text-2xl font-bold text-gray-800">Product Details</h1>
+          <button
+            class="text-gray-400 hover:text-gray-600 transition-colors duration-200 hover:cursor-pointer"
+            @click="$emit('fechar')"
+          >
+            X
+          </button>
+        </div>
+        <br />
+        <section class="flex">
+          <div
+            class="mr-10 bg-gray-50 border-2 border-gray-300 flex items-center"
+          >
+            <img :src="product.imageUrl" :alt="product.name" />
+          </div>
+          <div class="w-185 h-80">
+            <div>
+              <h1 class="text-3xl font-bold text-gray-800 mb-2">
+                {{ product.name }}
+              </h1>
+            </div>
+            <span class="text-sm text-gray-600"
+              >⭐ (4.2) • 127 reviews</span
+            >
+            <div>
+              <br />
+              <div class="text-4xl font-bold text-sky-600 mb-6">
+                $ {{ product.price.toFixed(2) }}
+              </div>
+              <p class="text-gray-600 leading-relaxed w-120">
+                {{ product.description }}
+              </p>
+              <br>
+            </div>
+            <div>
+              <div class="grid grid-cols-2 gap-4">
+                <div
+                  class="flex items-center gap-3 p-3 bg-sky-50 rounded-lg"
+                >
+                  🛡️
+                  <span class="text-sm text-gray-700">2 Year Warranty</span>
+                </div>
+                <div
+                  class="flex items-center gap-3 p-3 bg-sky-50 rounded-lg"
+                >
+                  🚚
+                  <span class="text-sm text-gray-700">Free Shipping</span>
+                </div>
+                <div
+                  class="flex items-center gap-3 p-3 bg-sky-50 rounded-lg"
+                >
+                  ↪️
+                  <span class="text-sm text-gray-700">30-Day Returns</span>
+                </div>
+                <div
+                  class="flex items-center gap-3 p-3 bg-sky-50 rounded-lg"
+                >
+                  🌟
+                  <span class="text-sm text-gray-700">Premium Quality</span>
+                </div>
+              </div>
+            </div>
+            <br />
+            <div class="border-t border-gray-200 pt-6">
+              <div class="grid grid-cols-2 gap-4 text-sm">
+                <div>
+                  <span class="text-gray-500">SKU:</span>
+                  <span class="ml-2 font-medium">#{{ product.id }}</span>
+                </div>
+                <div>
+                  <span class="text-gray-500">Stock:</span>
+                  <span
+                    v-if="product.stock"
+                    class="ml-2 font-medium text-green-600"
+                    >In Stock</span
+                  >
+                  <span v-else class="ml-2 font-medium text-red-600"
+                    >Out of Stock</span
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+import api from "../services/api";
+
+const props = defineProps(
+  {
+    visivel: { type: Boolean, required: true },
+    productId: { type: Number, default: null },
+    product: { type: Object, required: true },
+  },
+  watch(
+    () => props.visivel,
+    (novoValor) => {
+      console.log(
+        '3. Prop "visivel" recebida no filho com o valor:',
+        novoValor
+      );
+    }
+  )
+);
+
+defineEmits(["fechar"]);
+
+const product = ref(null);
+
+const pullData = async (id) => {
+  if (!id) return;
+  try {
+    const response = await api.get(`/api/products/${id}`);
+    product.value = response.data;
+  } catch (error) {
+    console.error(`Erro ao puxar dados do produto ${id}:`, error);
+  }
+};
+
+watch(
+  () => props.productId,
+  (newId) => {
+    if (newId) {
+      pullData(newId);
+    } else {
+      product.value = null;
+    }
+  }
+);
+</script>
